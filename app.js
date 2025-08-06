2cfa26c1c01c60113c5fa279f9b076d5a2c2e49a
Mkamil52020 <87297036+Mkamil52020@users.noreply.github.com>
2025-08-06 21:51:52 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const menuButtons = document.querySelectorAll('.menu-button');
 const modeSections = document.querySelectorAll('.mode-section');
 
+// Obiekt do przechowywania stanu aktywności każdego interwału
+const intervalStates = {};
+
 menuButtons.forEach(button => {
     button.addEventListener('click', () => {
         menuButtons.forEach(btn => {
@@ -22,4 +25,51 @@ menuButtons.forEach(button => {
             }
         });
     });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleCheckboxes = document.querySelectorAll('.toggle-checkbox');
+    const intervalButtons = document.querySelectorAll('.interval-name-button');
+
+    // inicjalizacja stanów
+    toggleCheckboxes.forEach(checkbox => {
+        const intervalName = checkbox.dataset.interval;
+        intervalStates[intervalName] = checkbox.checked;
+
+        // ustawienie początkowego stanu przycisku
+        const associatedButton = document.querySelector(`.interval-name-button[data-interval="${intervalName}"]`);
+        if (associatedButton) {
+            associatedButton.disabled = !intervalStates[intervalName];
+        }
+    });
+
+    toggleCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', (event) => {
+            const intervalName = event.target.dataset.interval;
+            intervalStates[intervalName] = event.target.checked;
+            
+            // zmiana stanu przycisku po kliknięciu
+            const associatedButton = document.querySelector(`.interval-name-button[data-interval="${intervalName}"]`);
+            if (associatedButton) {
+                associatedButton.disabled = !intervalStates[intervalName];
+            }
+        });
+    });
+});
+
+//resetowanie wyniku po zmianie trybów
+
+const scoreReset = () => {
+    const correctCount = document.querySelector('#correct-count');
+    correctCount.textContent = '0'
+    const wrongCount = document.querySelector('#wrong-count');
+    wrongCount.textContent = '0'
+};
+
+const startButton = document.getElementById('start-button');
+const nextButton = document.getElementById('next-button');
+const resetButton = document.getElementById('reset-button');
+
+resetButton.addEventListener('click', () => {
+    scoreReset();
 });
